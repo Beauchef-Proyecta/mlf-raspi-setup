d588877cab9c8c088471191049fd16a4c1229cd1
Francisco Casado <fcasado@ug.uchile.cl>
2021-11-25 00:43:51 -0300

    untracked scripts folder

--- scripts/pull.sh
+++
@@ -1,4 +0,0 @@
-cd /home/pi/mlf
-find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} reset --hard origin/main \;
-find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} stash \;
-find . -mindepth 1 -maxdepth 1 -type d -print -exec git -C {} pull \;
